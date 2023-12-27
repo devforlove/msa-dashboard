127e5866afb81192afae8c4448625dcd55ab7360
devforlove <inwookgusa@gmail.com>
2023-12-27 23:33:54 +0900

    [feature/#14] listener 추가

--- client-post-command/src/main/java/com/wook/top/postcommand/postquery/PostQueryClient.java
+++ client-post-command/src/main/java/com/wook/top/postcommand/postquery/PostQueryClient.java
@@ -1,12 +1,10 @@
 package com.wook.top.postcommand.postquery;
 
-import com.wook.top.postcommand.postquery.request.PostInfoResponse;
+import com.wook.top.postcommand.postquery.PostClientErrorDecoder.PostInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
 		name = "member-client",
